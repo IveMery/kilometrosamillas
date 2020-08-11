d1c35aa83678679ce33fa4621f43b8da1521fe5a
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:29:01 -0400

    kilometrosamillas

---
+++ index.js
@@ -0,0 +1,10 @@
+// Kilometros a millas
+
+
+alert ("Debes ingresar  la cantidad de kilómetros que quieras y lo pasaremos a millas")
+let kilometros = prompt("Ingresa los kilometros que desees convertir")
+
+
+let millas = (kilometros * 0.62137)
+
+alert  (`  ${kilometros} kilómetros son ${millas} Millas `)
